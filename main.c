7e8328058edac338e083267123b5b74e9d1648d6
tpltnt <tpltnt.github@dropcut.net>
2013-04-10 21:00:56 +0200

    all output pins, pwm setup

--- main.c
+++ main.c
@@ -2,7 +2,20 @@
   // Attiny25V
   #define F_CPU 8000000UL 
 #endif
+#include <avr/io.h>
 
 int main(void){
+  // all pins as outputs
+  DDRB = 0xff;
+
+  // set up pwm
+  TCCR0A = 0b00100010; // CTC-mode, OC0A disconnected, clear OC0B on match
+  TCCR0B = 0b00000001; // no prescaler
+  TCNT0 = 0;           // reset TCNT0
+  
+  /*
+    set OCR0A to 160 -> 20ms pwm time
+    OCR0B for down edge
+   */
   return 0;
 }
